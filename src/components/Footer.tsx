1835d1745ad63f30a4212560f699a7039646187f
João Victor da Silva Cardoso <joaovictordasilva0911@gmail.com>
2022-06-26 21:07:27 -0300

    subscribe-page-responsive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,9 @@ import { RocketLogo } from "./RocketLogo";
 
 export function Footer() {
   return(
-    <div className="w-full bg-gray-700 flex items-center">
-      <div className="w-full mx-auto max-w-[1180px] flex justify-between items-center py-6 border-t border-gray-600 self-center">
-        <div className="flex items-center gap-6">
+    <div className="w-full bg-gray-700 flex items-center px-6 xl:px-0">
+      <div className="w-full mx-auto max-w-[1180px] flex flex-col sm:flex-row gap-6 justify-between items-center py-6 border-t border-gray-600 self-center text-sm md:text-base">
+        <div className="flex flex-col sm:flex-row items-center gap-6">
           <RocketLogo />
           <span className="text-gray-300">
             Rocketseat - Todos os direitos reservados
--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -28,19 +28,19 @@ export function Subscribe(){
   
   return(
     <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-      <div className="w-full max-w-[1180px] flex items-center justify-between mt-20 mx-auto">
-        <div className="max-w-[640px] ">
+      <div className="w-full max-w-[1180px] flex flex-col items-center justify-between mt-10 px-28 mx-auto gap-6 md:flex-row sm:px-14 2xl:px-0 lg:mt-20">
+        <div className="max-w-[640px] flex flex-col items-center px-6 text-center md:items-start md:text-left ">
           <Logo />
 
-          <h1 className="mt-8 text-[2.5rem] leading-tight">
-            Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React JS</strong>
+          <h1 className="mt-8 text-[2.5rem] leading-tight text-3xl lg:text-4xl">
+            Construa uma <strong className="text-blue-500">aplicação completa</strong>, do zero, com <strong className="text-blue-500">React</strong>
           </h1>
-          <p className="mt-5 text-gray-200 leading-relaxed">
+          <p className="mt-5 text-gray-200 leading-relaxed text-sm lg:text-base">
             Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
           </p>
         </div>
 
-        <div className="p-8 bg-gray-700 border border-gray-500 rounded">
+        <div className="p-8 w-screen bg-gray-700 border-y border-gray-500 sm:rounded sm:w-full">
           <strong className="text-2xl mb-6 block">
             Inscreva-se gratuitamente
           </strong>
